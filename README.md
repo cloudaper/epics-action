a4abcb0eb6e00a59e008b28f1b013c756d0e0131
Kryštof Korb <krystof@korb.cz>
2020-05-02 22:12:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Epic issues for GitHub
+
+This action allows you to create epic issues on GitHub. Simply label an issue as `epic` and reference another issues in the task list:
+
+```markdown
+This is an epic issue. List of individual issues is below:
+
+- [ ] First issue #1
+- [ ] Another issue (#2)
+- [ ] Last issue #3 (not that important)
+```
+
+The action automatically updates the task list when referenced issue is closed (or reopened).
+
+## Inputs
+
+- **`github-token`**  
+  the GitHub token secret (use `${{secrets.GITHUB_TOKEN}}` in action YAML)  
+  _required_
+- **`epic-label-name`**  
+  name of the label you want to use for epic issues  
+  _default: `epic`_
+
+## Example usage
+
+```yaml
+name: Update epics
+on:
+  issues:
+    types: [opened, created, closed, reopened, deleted]
+jobs:
+  epics:
+    runs-on: ubuntu-latest
+    name: Update epic issues
+    steps:
+      - name: Run epics action
+        uses: cloudaper/epics-action@v1
+        with:
+          github-token: ${{secrets.GITHUB_TOKEN}}
+          epic-label-name: feature
+```
+
+## Development
+
+Install dependencies:
+
+```shell
+yarn
+```
+
+Lint & compile dependencies into `dist/index.js`:
+
+```shell
+yarn build
+```
---
+++ index.js
@@ -0,0 +1,66 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+async function getReferencedEpics({ octokit }) {
+  const epicLabelName = core.getInput('epic-label-name', { required: true });
+
+  const events = await octokit.issues.listEventsForTimeline({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: github.context.payload.issue.number,
+  });
+
+  const referencedEpics = events.data
+    .filter((item) => (item.event === 'cross-referenced' && item.source))
+    .filter((item) => item.source.issue.labels
+      .filter((label) => label.name.toLowerCase() === epicLabelName.toLowerCase()).length > 0);
+
+  return referencedEpics;
+}
+
+async function updateEpic({ octokit, epic }) {
+  const issueNumber = github.context.payload.issue.number;
+  const issueState = github.context.payload.issue.state;
+  const convertedIssueState = issueState === 'closed' ? 'x' : ' ';
+  const epicNumber = epic.source.issue.number;
+  let epicBody = epic.source.issue.body;
+
+  const pattern = new RegExp(`- \\[[ |x]\\] .*#${issueNumber}.*`, 'gm');
+  const matches = epicBody.matchAll(pattern);
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const match of matches) {
+    epicBody = epicBody.replace(match[0], match[0].replace(/- \[[ |x]\]/, `- [${convertedIssueState}]`));
+  }
+
+  const result = await octokit.issues.update({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: epicNumber,
+    body: epicBody,
+  });
+
+  return result;
+}
+
+async function updateEpics({ octokit, epics }) {
+  return Promise.all(epics.map((epic) => updateEpic({ octokit, epic })));
+}
+
+async function run() {
+  try {
+    const token = core.getInput('github-token', { required: true });
+
+    const octokit = new github.GitHub(token, {
+      previews: ['mockingbird-preview'],
+    });
+
+    const epics = await getReferencedEpics({ octokit });
+    await updateEpics({ octokit, epics });
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+run();
